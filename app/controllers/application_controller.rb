25ed7f9537c5345bd24ddd888f45683dc75d2381
viankarivera <viankalrivera@gmail.com>
2020-06-24 13:48:16 -0400

    app controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     erb :welcome
-  end
+  end 
 
 end
---
+++ app/controllers/pin_controller.rb
@@ -0,0 +1,29 @@
+class PinController < ApplicationController
+    get '/pins' do
+        @pins = Pin.all 
+        erb :index
+    end 
+    
+    post '/pins' do 
+        @pin = Pin.new(params)
+        @pin.save
+        redirect "/pins"
+    end 
+    
+    get '/pins/new' do 
+        erb :new 
+    end 
+    
+    get '/pins/:id' do 
+    end 
+    
+    get '/pins/:id/edit' do 
+    end 
+    
+    patch '/pins/:id' do 
+    end 
+    
+    delete '/pins/:id' do 
+    end 
+
+end 
