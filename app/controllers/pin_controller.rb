db86ded0eb1bca29f8e04e08ce7d2f75a8e8b296
viankarivera <viankalrivera@gmail.com>
2020-06-27 14:58:42 -0400

    changes

--- app/controllers/pin_controller.rb
+++ app/controllers/pin_controller.rb
@@ -1,18 +1,89 @@
 class PinController < ApplicationController
 
+    get '/pin' do 
+        if logged_in?
+            @pin = Pin.all 
+            erb :'pin/pins'
+        else 
+            redirect to '/login'
+        end 
+    end 
+
     get '/pin/new' do 
-        erb :'pin/new'  
+        if logged_in?
+            erb :'pin/new'
+        else
+            redirect to '/login'
+        end 
+    end 
+
+    post '/pin' do 
+        if logged_in?
+            if params[:content] == ""
+                redirect to "/pin/new"
+            else 
+                @pin = current_user.pins.build(content: params[:content])
+                if @pin.save
+                    redirect to "/pin/#{@pin.id}"
+                else
+                    redirect to "/pin/new"
+                end 
+            end 
+        else 
+            redirect to '/login'
+        end 
+    end 
+
+    get '/pin/:id/edit_pin' do 
+        if logged_in?
+            @pin = Pin.find_by_id(params[:id])
+            if @pin && @pin.user == current_user 
+                erb :'pin/edit_pin'
+            else 
+                redirect to '/pin'
+            end 
+        else 
+            redirect to '/login'
+        end 
+    end 
+
+    patch '/pin/:id' do 
+        if logged_in?
+            if params[:content] == ""
+                redirect to "/pin/#{params[:id]}/edit_pin"
+            else
+                @pin = Pin.find_by_id(params[:id])
+                if @pin && @pin.user == current_user 
+                    if @pin.update(content: params[:content])
+                        redirect to "/pin/#{@pin.id}"
+                    else
+                        redirect to "/pin/#{@pin.id}/edit_pin"
+                    end 
+                else
+                    redirect to '/pins'
+                end 
+            end 
+        else
+            redirect to '/login'
+        end 
     end 
     
     delete '/pin/:id/delete' do 
-        @pin = Pin.find_by_id(params[:id])
-        @pin.destroy
-        redirect '/pin'
+        if logged_in?
+            @pin = Pin.find_by_id(params[:id])
+            if @pin && @pin.user == current_user
+            @pin.delete
+            end 
+            redirect to '/pins'
+        else
+            redirect to '/login'
+        end 
     end 
 
-    private 
-    def pin_params
-        {artist: params[:artist], user_id: params[:user_id], series: params[:series], price: params[:price]}
-    end 
+
+    #private 
+    #def pin_params
+     #   {artist: params[:artist], user_id: params[:user_id], series: params[:series], price: params[:price]}
+    #end 
 
 end 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,29 +4,57 @@ class UserController < ApplicationController
     if !logged_in?
       erb :'user/signup'
     else
-      erb :'/index'
+      erb :'/show'
     end 
 
+  end 
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else 
+      @user = User.new(:username => params[:username])
+      @user.save
+      session[:id] = @user.id 
+      redirect to '/show'
+    end 
   end 
 
   get '/login' do 
-    if logged_in?
-      redirect "/user/#{@user.id}"
-      erb :"user/user"
-    else 
+    if !logged_in?
       erb :'user/login'
+    else 
+      redirect to '/show'
     end 
   end
-  
-  post '/signup' do 
-    @user = User.new(:username => params[:username])
-    @user.save
-    session[:id] = @user.id 
-    redirect "/user/#{@user.id}"
+
+  post '/login' do 
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect to "/show"
+    else 
+      redirect to '/signup'
+    end 
   end 
 
-  get '/user/show' do 
-    erb :show
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/show' do
+    if logged_in?
+    erb :'user/show'
+    else 
+      redirect to '/show'
+    end 
   end 
+
+
 end 
 
