92f3b951e407402727a34d16fbf6714db0e38fdb
viankarivera <viankalrivera@gmail.com>
2020-06-28 11:43:39 -0400

    good fixes

--- app/controllers/pin_controller.rb
+++ app/controllers/pin_controller.rb
@@ -2,13 +2,20 @@ require './config/environment'
 
 class PinController < ApplicationController
 
-    get '/pins' do 
-        @pins = Pin.all 
+    get '/pins' do  
         erb :'pins/pins'
     end 
 
     post '/pins' do 
-        erb :'pins'
+        erb :'pins/pins'
+    end 
+    
+    get '/pins/edit_pin' do
+        erb :'pins/edit_pin'
+    end 
+
+    post '/pins/edit_pin' do 
+       erb :'pins/edit_pin'
     end 
 
     get '/pins/new' do 
@@ -16,54 +23,11 @@ class PinController < ApplicationController
     end 
 
     post '/pins/new' do 
-        erb :"/pins/show_pins"
+        erb :"pins/pins"
     end 
 
-    get '/pins/:id/edit_pin' do 
-        if logged_in?
-            @pins = Pin.find_by_id(params[:id])
-            if @pins && @pins.user == current_user 
-                erb :'pins/edit_pin'
-            else 
-                redirect to '/pins'
-            end 
-        else 
-            redirect to '/login'
-        end 
-    end 
-
-    patch '/pins/:id' do 
-        if logged_in?
-            if params[:content] == ""
-                redirect to "/pins/#{params[:id]}/edit_pin"
-            else
-                @pins = Pin.find_by_id(params[:id])
-                if @pins && @pins.user == current_user 
-                    if @pins.update(content: params[:content])
-                        redirect to "/pins/#{@pin.id}"
-                    else
-                        redirect to "/pins/#{@pin.id}/edit_pin"
-                    end 
-                else
-                    redirect to '/pins'
-                end 
-            end 
-        else
-            redirect to '/login'
-        end 
-    end 
     
-    delete '/pins/:id' do 
-        #if logged_in?
-        @pins = Pin.find(params[:id])
-        user = @pins.user 
-        if user == current_user 
-            @pins.destroy
-            redirect to "/pins"
-        else
-            redirect to "/pins"  
-        end   
-    end 
+
 
 
     private 
