ac5914cbc0619e9f65173241594123d25b3e0d96
viankarivera <viankalrivera@gmail.com>
2020-07-07 16:28:02 -0400

    continued changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,5 +23,15 @@ class ApplicationController < Sinatra::Base
     erb :index
   end 
 
+  helpers do 
+    def is_logged_in?
+      !!session[:user_id]
+    end 
+
+    def current_user
+      @current_user ||=User.find_by_id(session[:user_id])
+    end 
+  end 
+
   
 end
