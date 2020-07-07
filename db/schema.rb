fb963b71094b80d0cd81635209330c8ba5be989a
viankarivera <viankalrivera@gmail.com>
2020-07-07 14:55:50 -0400

    start of fixing

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20200623215850) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "series"
+    t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
